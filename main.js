f73e4180114df74a6c0d07c0f08eb249775c31d5
gfulbright <gfulbright@hq.sportsinc.com>
2023-05-24 09:01:56 -0600

    changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            description: "They're socks.",
             image: './assets/images/socks_green.jpg',
             // solution
             url: 'https://www.vuemastery.com/'
